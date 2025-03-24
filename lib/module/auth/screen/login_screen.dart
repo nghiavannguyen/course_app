1a2c642027b81cba95d50999817a3979205f7aed
Văn Nghĩa <nghiazorovn2304@gmail.com>
2025-03-24 17:00:45 +0700

    set up dio link

--- lib/module/auth/screen/login_screen.dart
+++ lib/module/auth/screen/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:course_app/core/di/service_locator.dart';
+import 'package:course_app/core/network/dio_client.dart';
 import 'package:course_app/navigation/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -97,8 +99,15 @@ class LoginScreen extends StatelessWidget {
               SizedBox(
                 width: double.infinity,
                 child: ElevatedButton(
-                  onPressed: () {
-                    Get.toNamed(Routes.root);
+                  onPressed: () async {
+                    // Get.toNamed(Routes.root);
+                    final res = await sl<DioClient>().dio.post('/auth/login',
+                        data: {
+                          "username": "ad@gmail.com",
+                          "password": "nghia1"
+                        });
+                    print(res.statusCode);
+                    print(res.statusMessage);
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: theme.primaryColor,
