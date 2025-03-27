6ef2cd1fb90055ca9dd26ff2f2c1981403db0f83
NghiaKUTE <vutrongnghia02022002@gmail.com>
2025-03-27 11:00:45 +0700

    push code

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -2,10 +2,11 @@ import 'package:dio/dio.dart';
 
 class DioClient {
   late final Dio dio;
+  String ipV4 = "172.29.0.1";
   DioClient() {
     dio = Dio(
       BaseOptions(
-          baseUrl: 'http://172.24.224.1:3001/api/v1',
+          baseUrl: 'http://$ipV4:3001/api/v1',
           connectTimeout: Duration(seconds: 20),
           receiveTimeout: Duration(seconds: 20),
           headers: {'Content-Type': 'application/json'}),
